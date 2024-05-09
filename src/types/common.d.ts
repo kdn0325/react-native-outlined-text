edce63d25b737c1c2283440d76e7a0e6ff0b9921
kdn0325 <dn10003@gmail.com>
2024-05-09 23:45:12 +0900

    feat: update the default type to the declare type

---
+++ src/types/common.d.ts
@@ -0,0 +1,15 @@
+type TextAlign = 'auto' | 'left' | 'right' | 'center' | 'justify';
+
+type FontWeight =
+  | 'normal'
+  | 'bold'
+  | '100'
+  | '200'
+  | '300'
+  | '400'
+  | '500'
+  | '600'
+  | '700'
+  | '800'
+  | '900'
+  | undefined;
